be1bacca857669ef644fa4050e3a767b11353abb
归尘若是过客人 <2338287783@qq.com>
2021-05-11 11:01:38 +0800

    first commit

---
+++ config/defaultSettings.ts
@@ -0,0 +1,24 @@
+import { Settings as ProSettings } from '@ant-design/pro-layout';
+
+type DefaultSettings = Partial<ProSettings> & {
+  pwa: boolean;
+};
+
+const proSettings: DefaultSettings = {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'side',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: '青融商城',
+  pwa: false,
+  siderWidth:280,
+  iconfontUrl: '',
+};
+
+export type { DefaultSettings };
+
+export default proSettings;
---
+++ config/routes.ts
@@ -0,0 +1,45 @@
+﻿export default [
+  {
+    path: '/',
+    component: '../layouts/BlankLayout',
+    routes: [
+      {
+        path: '/user',
+        component: '../layouts/UserLayout',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './User/login',
+          },
+        ],
+      },
+      {
+        path: '/',
+        component: '../layouts/SecurityLayout',
+        routes: [
+          {
+            path: '/',
+            component: '../layouts/BasicLayout',
+            routes: [
+              {
+                path: '/',
+              },
+             
+              
+              {
+                component: './404',
+              },
+            ],
+          },
+          {
+            component: './404',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    component: './404',
+  },
+];
